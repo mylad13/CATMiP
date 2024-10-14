e2e384d35e65b444dc069c22be6f349717d02b5d
mylad13 <mfarjadnasab@gmail.com>
2024-10-14 11:04:49 -0400

    fixed errors

--- matsar/scripts/train_gridworld.sh
+++ matsar/scripts/train_gridworld.sh
@@ -19,10 +19,10 @@ do
     --n_rollout_threads 128 --num_mini_batch 1 --num_env_steps 250000000 --ppo_epoch 10 \
     --lr 1e-4 --use_linear_lr_decay --max_steps 200 --agent_view_size 7 --local_step_num 10 \
     --astar_cost_mode normal --grid_size 20 --use_full_comm  --entropy_coef 0.01 \
-    --clip_param 0.05 --gamma 1 --asynch \
+    --clip_param 0.05 --gamma 1 --asynch --use_wandb \
     --trajectory_forget_rate 0.9101 --goal_history_decay 0 \
     --n_head 1 --n_embd 192 --n_block 1 --action_size 3 \
-    --n_agent_types 2 --agent_types_list 0 1 10 --detect_traces --use_intrinsic_reward --use_action_masking  \
-     #removed  --use_agent_obstacle  --use_wandb  --block_doors --block_chance 0.3 --use_time_penalty --use_energy_penalty
+    --n_agent_types 2 --agent_types_list 0 1 10 --detect_traces --use_intrinsic_reward --use_action_masking --use_agent_obstacle  \
+     #removed --use_time_penalty --use_energy_penalty
     # 
 done
